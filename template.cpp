6f8d1cc216b7fb2a975d14745d9d0cfd2b754576
LipArcanjo <luanicaroarcanjo@gmail.com>
2022-12-01 11:19:45 -0300

    adicionando template

---
+++ template.cpp
@@ -0,0 +1,41 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define dbg(x) cout << "[" << #x << "]: " << x << endl;
+
+using ll = long long;
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MAXN = 2e5 + 5;
+const int MOD = 1e9+7; //998244353;
+const int INF = 0x3f3f3f3f;
+const ll INF64 = ll(4e18) + 5;
+
+void solve(){
+}
+
+int main(){
+
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
