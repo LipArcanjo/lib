93bd80de0a713bb5a7fb107a3b4c2cfca7357cc5
LipArcanjo <luanicaroarcanjo@gmail.com>
2023-11-22 01:25:47 -0300

    remove spaces

--- extra/template.cpp
+++ extra/template.cpp
@@ -1,26 +1,18 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC target("avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
-
 #include <bits/stdc++.h>
 using namespace std;
-
 #define pb push_back
 #define mp make_pair
 #define fi first
 #define se second
-
+#define int long long
 void dbg_out() { cerr << endl; }
 template <typename H, typename... T>
 void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
 #define dbg(...) { cerr << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__); }
-
 using ll = long long;
-using pi = pair<int,int>;
-using pl = pair<ll,ll>;
-using vi = vector<int>;
-using vl = vector<ll>;
-
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 const int MAXN = 2e5 + 5;
@@ -31,7 +23,7 @@ const ll INF64 = ll(4e18) + 5;
 void solve(){
 }
 
-int main(){
+signed main(){
 
     ios::sync_with_stdio(false); cin.tie(NULL);
 
