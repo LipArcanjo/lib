61941e7aa45b2cc2122b91be8882a41ffd1a9b9e
LipArcanjo <luanicaroarcanjo@gmail.com>
2023-12-20 14:08:58 -0300

    add reference to sum

--- string/stringHash.cpp
+++ string/stringHash.cpp
@@ -26,7 +26,7 @@ int inv(int a){
     return pw(a,MOD-2);
 }
  
-int gera_a(){
+int gen_a(){
 	int X = MOD/2;
 	int L = rng();
 	L %= MOD/4;
@@ -34,8 +34,9 @@ int gera_a(){
 	return X+L;
 }
 
-void process(int a, string &s, vector<int> &pwa, vector<int> &invpwa){
+void process(int a, string &s, vector<int> &pwa, vector<int> &invpwa, vector<int> &sum){
 	int n = s.size();
+	pwa = invpwa = sum = vector<int>(n,0);
 	pwa[0] = 1;
 	invpwa[0] = 1;
 	int inva = inv(a);
@@ -43,7 +44,6 @@ void process(int a, string &s, vector<int> &pwa, vector<int> &invpwa){
 		pwa[i] = mul(pwa[i-1],a);
 		invpwa[i] = mul( invpwa[i-1],inva);
 	}
-	vector<int> sum(n);
 	for(int i = 0; i < n; i++){
 		sum[i] = add( mul(int(s[i]),pwa[i]), (i ? sum[i-1] : 0));
 	}
